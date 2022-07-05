1436d3fbf4ad02b667a3c2d982fd985267e38f2b
ximuli <ximuli666@163.com>
2022-07-05 22:45:19 +0800

    实现items分页

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::ItemsController < ApplicationController
+  def index
+    # items = Item.page(params[:page]).per(100)
+    items = Item.page params[:page]
+    render json: { resources: items, pager: {
+      page: params[:page],
+      per_page: 10,
+      count: Item.count
+    }}
+  end
+  
+  def create
+    item = Item.new amount: 1
+    if item.save
+      render json: { resource: item }
+    else
+      render json: { errors: item.errors }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post '/users', to: 'users#create'
-  get '/users/:id', to: 'users#show'
+
+  # post '/users', to: 'users#create'
+  # get '/users/:id', to: 'users#show'
 
   namespace :api do
     namespace :v1 do
